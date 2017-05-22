439ccc90799e8d69b56817097f60f42bd34e25cb
Phil <philmudavanhu@gmail.com>
2017-05-22 03:26:36 +0200

    Aufgabe 5

---
+++ build/source/CMakeFiles/aufgabe5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe5.dir/aufgabe5.cpp.o"
+  "aufgabe5.pdb"
+  "aufgabe5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aufgabe5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/aufgabe5.cpp
@@ -0,0 +1,48 @@
+# define CATCH_CONFIG_RUNNER
+# include <catch.hpp>
+# include <cmath>
+# include <algorithm>
+#include <iostream>
+
+
+bool isnt_multiple_of_3(int i){
+    return ((i%3)!=0);
+}
+
+bool is_multiple_of_3(int i){
+    return ((i%3)==0);
+}
+
+TEST_CASE ("filter alle vielfache von drei", "[erase]")
+{
+
+//using set to get the different numbers from 1 to 100 
+std::cout <<"  " <<std::endl; 
+std::cout <<" ------------------------------Aufgabe 5 vector with random number 1 to 100----------------------------------" <<std::endl; 
+
+std :: vector <int> v0(100);
+	for (auto& v : v0){
+	v = std::rand()%101;
+	std::cout << v <<" ";
+}
+
+
+std::cout <<"  " <<std::endl; 
+std::cout <<" -----------------------------------------Numbers that are Divisible by 3-------------------------------------" <<std::endl;
+
+v0.erase(std::remove_if(v0.begin(), v0.end(), isnt_multiple_of_3), v0.end());
+	for (auto& v : v0){
+	std :: cout << v <<" ";
+}
+
+std::cout <<"  " <<std::endl; 
+std::cout <<" ------------------------------------------------TEST_CASE Assertion-------------------------------------------" <<std::endl;
+
+REQUIRE(std::all_of(v0.begin(), v0.end(), is_multiple_of_3));
+
+
+}
+int main(int argc, char* argv[])
+{
+return Catch::Session().run(argc, argv);
+}
